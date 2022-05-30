9dc4bce1f4b57b617fae66be42f95f2172848b57
parnabas <wen_zhong@outlook.com>
2022-05-30 19:34:04 +0800

    trying to open excel file but to no avail

--- trading-signaller/trading_signaller.py
+++ trading-signaller/trading_signaller.py
@@ -1,13 +1,40 @@
+import win32com.client
+import os
+import pandas as pd
 from tradingview_ta import TA_Handler, Interval, Exchange
 import tradingview_ta
 import time
 
-
-
 # Example output: {"RECOMMENDATION": "BUY", "BUY": 8, "NEUTRAL": 6, "SELL": 3}
 
+file_name = "C:/Users/wen_z/Documents/GitHub/trading-signaller/trading-signaller/text.xlsx"
 start_time = time.time()
 seconds = 1
+
+
+
+# Create an instance of the Excel Application & make it visible.
+ExcelApp = win32com.client.GetActiveObject("Excel.Application")
+ExcelApp.Visible = True
+
+print(os.listdir('.'))
+# Open the desired workbook
+workbook = ExcelApp.Workbooks.Open("whyisntthisworking.xlsx")
+
+# Take the data frame object and convert it to a recordset array
+rec_array = data_frame.to_records()
+
+# Convert the Recordset Array to a list. This is because Excel doesn't recognize 
+# Numpy datatypes.
+rec_array = rec_array.tolist()
+
+# It will look something like this now.
+# [(1, 'Apple', Decimal('2'), 4.0), (2, 'Orange', Decimal('3'), 5.0), (3, 'Peach', 
+# Decimal('5'), 5.0), (4, 'Pear', Decimal('6'), 5.0)]
+
+# set the value property equal to the record array.
+ExcelApp.Range("F2:I5").Value = rec_array
+
 
 while True:
     current_time = time.time()
